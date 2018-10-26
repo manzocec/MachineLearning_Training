e696f7e0a13fd101c7ae542272ed224987b95223
ELCA_manzocec <cecilia.manzoni@elca.ch>
2018-10-26 17:39:52 +0200

    Updating results Lab03 and Lab04

--- Lab04_BiasVariance/least_squares.py
+++ Lab04_BiasVariance/least_squares.py
@@ -5,13 +5,14 @@ Least Square
 """
 
 import numpy as np
+from costs import compute_mse
 
 
 def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    """calculate the least squares solution."""
+    A = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    w = np.linalg.solve(A, b)
+    mse_err = compute_mse(y, tx, w)
+    
+    return w, mse_err
--- Lab04_BiasVariance/ridge_regression.py
+++ Lab04_BiasVariance/ridge_regression.py
@@ -9,8 +9,8 @@ import numpy as np
 
 def ridge_regression(y, tx, lamb):
     """implement ridge regression."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # ridge regression: TODO
-    # ***************************************************
-    raise NotImplementedError
+    A = tx.T.dot(tx) + 2*lamb*np.shape(tx)[0]*np.eye(np.shape(tx)[1])
+    b = tx.T.dot(y)
+    
+    return np.linalg.solve(A, b)
+
